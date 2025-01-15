d0f1940b3f9a82b56811fbb31f8717d8f2be1b43
duduofficiel <duduofficiell@gmail.com>
2025-01-15 09:55:07 +0100

    Initial commit

---
+++ src/Controller/PaiementController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PaiementController extends AbstractController
+{
+    #[Route('/paiement-page', name: 'Paiement')]
+    public function index(): Response
+    {
+        return $this->render('paiement/index.html.twig', [
+            'controller_name' => 'PaiementController',
+        ]);
+    }
+}
